5ffc5f15436e1209d8879c8642528912f3b703a3
lotuskolobok <lotus.kolobok@gmail.com>
2023-12-25 22:52:33 +0200

    assistant

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from collections import UserDict
 from datetime import datetime
 from datetime import date
 
+from pathlib import Path
+
 import re
 import pickle
-import cmd
+import sort
+import scan
+import os
+import notes
+
 
 def validate_email(email):
     try:
@@ -68,6 +74,7 @@ class Phone(Field):
         return self.value
 
 
+
 class Birthday(Field):
 
     @property
@@ -97,20 +104,28 @@ class Email(Field):
     def __str__(self):
         return self.value
 
+
+class Home(Field):
+    @property
+    def value(self):
+        return self._Field__value
+
+    @value.setter
+    def value(self, value):
+        self._Field__value = value
+
 class Record:
-    def __init__(self, name, birthday=None):
+    def __init__(self, name, birthday=None, home=None):
         self.name = Name(name)
         self.phones = []
         self.birthday = Birthday(birthday)
         self.emails = []
+        self.home = Home(home)
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}, e-mails: {'; '.join(p.value for p in self.emails)}"
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}, e-mails: {'; '.join(p.value for p in self.emails)}, address: {self.home}"
 
-    def show_birthday(self):
-        return f"Contact name: {self.name.value}, birthday: {self.birthday.value}"
-
-
+    
     def days_to_birthday(self):
         if self.birthday.value is None:
             delta_days = 0
@@ -132,9 +147,16 @@ class Record:
                 pass
         
         if delta_days > 0:
-            return f'Birthday is coming in: {delta_days} days'
+            return delta_days
         else:
             return ''
+        
+    def show_birthday(self):
+        if self.birthday.value:
+            return f"Contact name: {self.name.value}, birthday: {self.birthday.value}, left days: {self.days_to_birthday()}"
+        else:
+            return f"Contact name: {self.name.value}, birthday: {self.birthday.value}"
+        
 
     def add_email(self, email: str):
         tmp = validate_email(email)
@@ -245,10 +267,11 @@ class AddressBook(UserDict):
     def load(self):
         try:
             with open('my_book.txt', 'rb') as file:
-                self = pickle.load(file)
-                
+                self.data = pickle.load(file)
         except:
             pass
+                
+        
     
     # сериалізація адресної книги при виході з програми
     def exit(self):
@@ -259,112 +282,273 @@ class AddressBook(UserDict):
 # ----------------------------------------------------------------------------------------------------------
 
 
-def Examle():
+# def Examle():
 
-    print('*' * 100)
+    # print('*' * 100)
 
-    # Створення нової адресної книги
-    book = AddressBook()
+    # # Створення нової адресної книги
+    # book = AddressBook()
 
-    # Створення запису для John
-    john_record = Record("John", "2000-10-05")
+    # # Створення запису для John
+    # john_record = Record("John", "2000-10-05")
 
-    john_record.add_email("bobik@dog.yes")
-    john_record.add_email("murzik@cat.yes")
+    # john_record.add_email("bobik@dog.yes")
+    # john_record.add_email("murzik@cat.yes")
 
-    john_record.add_phone("1234567890")
-    john_record.add_phone("5555555555")
+    # john_record.add_phone("1234567890")
+    # john_record.add_phone("5555555555")
 
-    # Додавання запису John до адресної книги
-    book.add_record(john_record)
+    # # Додавання запису John до адресної книги
+    # book.add_record(john_record)
 
-    # Створення та додавання нового запису для Jane
-    jane_record = Record("Jane", "200002-20")
-    jane_record.add_email("dfdf")
-    jane_record.add_phone("9876543210")
-    book.add_record(jane_record)
+    # # Створення та додавання нового запису для Jane
+    # jane_record = Record("Jane", "200002-20")
+    # jane_record.add_email("dfdf")
+    # jane_record.add_phone("9876543210")
+    # book.add_record(jane_record)
 
-    # Створення та додавання нового запису для Sara
-    sara_record = Record("Sara")
-    sara_record.add_phone("5566997711")
-    book.add_record(sara_record)
-    sara_record.birthday = Birthday("1976-07-14")
+    # # Створення та додавання нового запису для Sara
+    # sara_record = Record("Sara")
+    # sara_record.add_phone("5566997711")
+    # book.add_record(sara_record)
+    # sara_record.birthday = Birthday("1976-07-14")
 
-    # Виведення всіх записів у книзі
-    for record in book.data.items():
-        print(record)
+    # # Виведення всіх записів у книзі
+    # for record in book.data.items():
+    #     print(record)
 
-    # Знаходження та редагування телефону для John
-    john = book.find("John")
-    john.edit_phone("1234567890", "1112223333")
+    # # Знаходження та редагування телефону для John
+    # john = book.find("John")
+    # john.edit_phone("1234567890", "1112223333")
 
-    # Знаходження та редагування email для John
-    john = book.find("John")
-    john.edit_email("bobik@dog.yes", "sharik@dog.yes")
+    # # Знаходження та редагування email для John
+    # john = book.find("John")
+    # john.edit_email("bobik@dog.yes", "sharik@dog.yes")
 
-    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+    # print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
 
-    # Пошук конкретного телефону у записі John
-    found_phone = john.find_phone("5555555555")
-    print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+    # # Пошук конкретного телефону у записі John
+    # found_phone = john.find_phone("5555555555")
+    # print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
 
-    # Пошук за збігом в імені або номері телефона
-    f = 'sa'
-    print(f'-----Пошук за [{f}]--------')
-    f = book.find_record(str(f))
-    for name, record in f.items():
-        print(record)
-    print('-' * 30)
+    # # Пошук за збігом в імені або номері телефона
+    # f = 'sa'
+    # print(f'-----Пошук за [{f}]--------')
+    # f = book.find_record(str(f))
+    # for name, record in f.items():
+    #     print(record)
+    # print('-' * 30)
 
-    # Виведення дати народження та кількості днів до дати народження
-    print(jane_record.show_birthday(), jane_record.days_to_birthday())
-    print(john_record.show_birthday(), john_record.days_to_birthday())
-    print(sara_record.show_birthday(), sara_record.days_to_birthday())
+    # # Виведення дати народження та кількості днів до дати народження
+    # print(jane_record.show_birthday(), jane_record.days_to_birthday())
+    # print(john_record.show_birthday(), john_record.days_to_birthday())
+    # print(sara_record.show_birthday(), sara_record.days_to_birthday())
 
-    # Видалення запису Jane
-    # book.delete("Jane")
+    # # Видалення запису Jane
+    # # book.delete("Jane")
 
-    book.exit()
+    # book.exit()
 
 if __name__ == "__main__":
 
-    Examle()
+    #Examle()
+
+    my_book = None
+    my_book = AddressBook()
+    my_book.load()
+
+    
 
     while True:
-        my_book = None
+        
+        result = False
+        
+        command = input('Input command or "?" for help: > ').lower().strip()
+        print('*' * 20, f'{command}', '*' * 20)
 
-        my_book = AddressBook().load()
-        #book = book.load()
-
-        command = input('Input command or "?" for help: ')
-
-        if command.lower() == "end":
-            print(my_book.exit())
+        if command == "end" or command == "exit":
+            my_book.exit()
+            print ('Bye-Bye!')
             break
         
-        elif command == 'add_user':
-            name = input('    Input name: ')
+        elif command == 'add_contact':
+            name = input('Input name for contact: ').strip()
             if name != '':
-                birthday = input('    Input bithday in format [yyyy-mm-dd]: ')
+                birthday = input('Input bithday in format [yyyy-mm-dd]: ').strip()
                 record = Record(name, birthday)
+
+                my_book.add_record(record)
+                
+                result = True
+
             else:
                 print("Name can't be empty. Try again.")
         
-        elif command == 'show_book':
-            # Виведення всіх записів у книзі
-            if my_book:
-                for record in my_book.data.items():
-                    print(record)
+        elif command == 'del_contact':
+            name = input('Input name for contact: ').strip()
+            if name != '':
+                my_book.delete(name)
+                result = True
+
+        elif command == 'add_address':
+            name = input('Input contact name: ').strip()
+            if name != '':
+                record = my_book.find(name)
+                if record:
+                    home = input('Input address: ').strip()
+                    record.home = home
+                    result = True
+                else:
+                    print('Name not found.')
+
+        elif command == 'add_birthday':
+            name = input('Input contact name: ').strip()
+            if name != '':
+                record = my_book.find(name)
+                if record:
+                    birthday = input('Input birthday in format yyyy-mm-dd: ').strip()
+                    record.birthday.value = birthday
+                    result = True
+                else:
+                    print('Name not found.')
+
+        elif command == 'add_email':
+            name = input('Input contact name: ').strip()
+            if name != '':
+                record = my_book.find(name)
+                if record:
+                    email = input('Input email: ').strip()
+                    record.add_email(email)
+                    result = True
+                else:
+                    print('Name not found.')
+        
+        elif command == 'edit_email':
+            name = input('Input contact name: ').strip()
+            if name != '':
+                record = my_book.find(name)
+                if record:
+                    old_email = input('Input old email: ').strip()
+                    new_email = input('Input new email: ').strip()
+                    record.edit_email(old_email, new_email)
+                    result = True
+                else:
+                    print('Name not found.')
+        
+        elif command == 'add_phone':
+            name = input('Input contact name: ').strip()
+            if name != '':
+                record = my_book.find(name)
+                if record:
+                    phone = input('Input phone number: ').strip()
+                    record.add_phone(phone)
+                    result = True
+                else:
+                    print('Name not found.')
+        
+        elif command == 'edit_phone':
+            name = input('Input contact name: ').strip()
+            if name != '':
+                record = my_book.find(name)
+                if record:
+                    old_phone = input('Input old phone number: ').strip()
+                    new_phone = input('Input new phone number: ').strip()
+                    record.edit_phone(old_phone, new_phone)
+                    result = True
+                else:
+                    print('Name not found.')
+
+        elif command == 'find_record':
+            find = input('Input symbols to search: ').strip()
+            if find != '':
+                records = my_book.find_record(find)
+                if len(records):
+                    print(f'-----Search by [{find}]--------')
+                    for name, record in records.items():
+                        print(record)
+                    print('*' * 0)
+                    result = True
+                else:
+                    print('Тo match found.')
 
         
+        elif command.startswith('show_birthdays'):
+            days = input('Input max days to birthdays: ')
+            if days.isdigit():
+                days = int(days)
+                if my_book:
+                    for name, record in my_book.data.items():
+                        delta_days = record.days_to_birthday()
+                        if delta_days:
+                            if int(delta_days) < days:
+                                print(record.show_birthday())
+                    result = True
+            else:
+                print('Incorect value of days count')
+
+
+        elif command == 'show_book':
+            if my_book:
+                for name, record in my_book.data.items():
+                    print(record)
+                    result = True
+        
+        elif command == 'scan_folder':
+        
+            folder = input('Input folder name for scaning: ') 
+
+            if os.path.exists(folder):
+                folder = Path(folder)
+                scan.scan(folder)
+                scan.scan_result()
+                result = True
+            else:
+                print ('Folder not found')
+
+        elif command == 'sort_folder':
+            
+            folder = input('Input folder name for scaning: ') 
+
+            if os.path.exists(folder):
+                folder = Path(folder)
+                sort.main(folder.resolve())
+                result = True
+            else:
+                print('Error sorting files')
+
+        elif command == 'notes':
+            notes.main()
+
         elif command == "?":
             commands = []
-            commands.append('end')
-            commands.append('add_user (name, [birthday]])')
             
+            commands.append('- [end] or [exit] - quit program')
+            
+            commands.append('- [add_contact]    - adding contact to book')
+            commands.append('- [del_contact]    - remove contact from book')
+            commands.append('- [add_address]    - adding address to contact')
+            commands.append('- [add_birthday]   - adding birthday to contact')
+            commands.append('- [add_email]      - adding email to contact')
+            commands.append('- [edit_email]     - edit email')
+            commands.append('- [add_phone]      - adding phone number to contact')
+            commands.append('- [edit_phone]     - edit phone number')
+            commands.append('- [find_record]    - search contact by symbols')
 
+            commands.append('- [show_birthdays] - show all birtdays in book')
+            commands.append('- [show_book]      - show all contacts in book')
+            
+            commands.append('- [scan_folder]    - scan folder')
+            commands.append('- [sort_folder]    - sort folder')
+            commands.append('- [notes]          - case of notes')
+
+            
             for c in commands:
                 print(f'{c}')
+        
+        print('*' * 50)
+        if result:
+            print('Result: OK')
+        print('*' * 50)
 
 
 
